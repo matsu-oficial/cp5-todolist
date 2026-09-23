29edd16564af19b75920b168a7f7ba894350927b
matsu-oficial <esports.matsu@gmail.com>
2026-09-22 22:40:42 -0300

    feat: navegacao entre lista e formulario integrada na MainActivity

--- app/src/main/java/matsu_oficial/com/github/todolist/MainActivity.kt
+++ app/src/main/java/matsu_oficial/com/github/todolist/MainActivity.kt
@@ -1,17 +1,13 @@
 package matsu_oficial.com.github.todolist
 
+import matsu_oficial.com.github.todolist.navigation.AppNavigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import matsu_oficial.com.github.todolist.ui.theme.FiaptodolistTheme
+import matsu_oficial.com.github.todolist.viewmodel.TarefaViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +15,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FiaptodolistTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val viewModel: TarefaViewModel = viewModel(
+                    factory = TarefaViewModel.factory(applicationContext)
+                )
+                AppNavigation(viewModel = viewModel)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FiaptodolistTheme {
-        Greeting("Android")
     }
 }
